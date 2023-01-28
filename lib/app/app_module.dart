85b534f64e868c767e40d293b48fb295294acde7
Raphael Pontes <raphael.pontes@condoconta.com.br>
2023-01-28 00:12:38 -0300

    module injectable

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,13 +1,21 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flut_micro_app_auth/flut_micro_app_auth.dart';
+import 'package:flut_micro_commons_auth/flut_micro_commons_auth.dart';
+import 'package:flut_micro_commons_client_https/flut_micro_commons_client_https.dart';
 
 class AppModule extends Module {
   @override
   final List<Bind> binds = [];
 
   @override
+  List<Module> imports = [
+    ClientHttpsModule(),
+    AuthModule(),
+  ];
+
+  @override
   final List<ModularRoute> routes = [
     // ModuleRoute('/', module: AuthModule()),
-    ModuleRoute('/', module: AuthModule()),
+    ModuleRoute('/', module: AppAuthModule()), // auth
   ];
 }
