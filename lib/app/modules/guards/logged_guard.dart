82929d1e78235dcda847b386e18dd6f46108f63b
Raphael Pontes <raphael.pontes@condoconta.com.br>
2023-01-30 22:00:11 -0300

    Removing Code Coupling

--- lib/app/modules/guards/logged_guard.dart
+++ lib/app/modules/guards/logged_guard.dart
@@ -1,12 +1,12 @@
 import 'package:flut_micro_commons_dependencies/flut_micro_commons_dependencies.dart';
+import 'package:flut_micro_commons_shared/flut_micro_commons_shared.dart';
 
 class LoggedGuard extends RouteGuard {
   LoggedGuard() : super(redirectTo: '/auth/');
 
   @override
   Future<bool> canActivate(String path, ModularRoute router) async {
-    final prefs = await SharedPreferences.getInstance();
-    var accessToken = prefs.getString('access_token');
+    var accessToken = await LocalStorage.get('access_token');
     return accessToken != null;
   }
 }
--- lib/app/modules/guards/unlogged_guard.dart
+++ lib/app/modules/guards/unlogged_guard.dart
@@ -1,12 +1,12 @@
 import 'package:flut_micro_commons_dependencies/flut_micro_commons_dependencies.dart';
+import 'package:flut_micro_commons_shared/flut_micro_commons_shared.dart';
 
 class UnloggedGuard extends RouteGuard {
   UnloggedGuard() : super(redirectTo: '/');
 
   @override
   Future<bool> canActivate(String path, ModularRoute router) async {
-    final prefs = await SharedPreferences.getInstance();
-    var accessToken = prefs.getString('access_token');
+    var accessToken = await LocalStorage.get('access_token');
     return accessToken == null;
   }
 }
